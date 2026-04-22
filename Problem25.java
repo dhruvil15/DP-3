4806c6ecc9ded259216fc7e8b0e9e28e4ff9c906
dhruvil15 <76967484+dhruvil15@users.noreply.github.com>
2026-04-21 20:09:21 -0400

    Complete24_25

---
+++ Problem25.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int max = 0;
+        for(int num: nums) {
+            max = Math.max(num, max);
+        }
+
+        int[] arr = new int[max+1];
+        for(int num: nums) {
+            arr[num] += num;
+        }
+
+        int prev = arr[0];
+        int curr = Math.max(arr[0], arr[1]);
+
+        for (int i = 2; i <= max; i++) {
+            int temp = curr;
+            curr = Math.max(curr, arr[i]+prev);
+            prev = temp;
+        }
+
+        return curr;
+    }
+}
